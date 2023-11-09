c593806c52e731a9a93f228a0bdf1627d086eda1
Masoud Ramezani <masoudhra@gmail.com>
2023-11-09 16:20:00 +1300

    {}

--- src/Test.jsx
+++ src/Test.jsx
@@ -3,6 +3,7 @@ function Test() {
         <div>
             <p>This is a test component</p>
             <span>I am Masoud Ramezani</span>
+            <p> { Math.round(Math.random() * 100) } </p>
         </div>
     )
 }
