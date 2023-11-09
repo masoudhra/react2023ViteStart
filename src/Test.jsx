7c5ff6b8a296b13d5dfd4ff423001aa7ebacdf97
Masoud Ramezani <masoudhra@gmail.com>
2023-11-09 16:59:50 +1300

    adjasent tag <></>

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,11 +1,16 @@
 function Test() {
-    return(
-        <div>
-            <p>This is a test component</p>
-            <span>I am Masoud Ramezani</span>
-            <p> { Math.round(Math.random() * 100) } </p>
-        </div>
-    )
+  return (
+    <>
+      <div>
+        <p>This is a test component</p>
+        <span>I am Masoud Ramezani</span>
+        <p> {Math.round(Math.random() * 100)} </p>
+      </div>
+      <div>
+        <h3>This is a second div</h3>
+      </div>
+    </>
+  );
 }
 
 export default Test;
