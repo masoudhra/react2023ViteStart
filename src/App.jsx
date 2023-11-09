264f8c86c3b7d2824c319f727cbbe26a5388212c
Masoud Ramezani <masoudhra@gmail.com>
2023-11-09 15:55:12 +1300

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Test from "./Test.jsx";
+
 function App() {
   return (
     <div>
       <h1>React 2023</h1>
       <p>React review</p>
+      <Test />
     </div>
   );
 }
---
+++ src/Test.jsx
@@ -0,0 +1,10 @@
+function Test() {
+    return(
+        <div>
+            <p>This is a test component</p>
+            <span>I am Masoud Ramezani</span>
+        </div>
+    )
+}
+
+export default Test;
