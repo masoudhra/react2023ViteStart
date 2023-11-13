64e05dae6cc8a0ac1bae4272181c9071fec7e955
Masoud Ramezani <masoudhra@gmail.com>
2023-11-13 17:03:25 +1300

    key id in map

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,17 +1,22 @@
 // List rendering
 function CourseList() {
-  const courses = ["React", "Javascript", "Next.js", "Node"];
+  const courses = [
+    { id: 1, name: "React" },
+    { id: 2, name: "Javascropt" },
+    { id: 3, name: "Next.js" },
+    { id: 4, name: "Node.js" },
+  ];
 
   return (
     <div>
       <h3>Course List</h3>
       <ul>
-        {courses.map((course, index) => (
-          <li key={index}>{course}</li>
+        {courses.map((course) => (
+          <li key={course.id}>{course.name}</li>
         ))}
       </ul>
     </div>
   );
 }
-                                                                                 
+
 export default CourseList;
