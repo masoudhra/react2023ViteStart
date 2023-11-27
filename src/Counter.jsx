80db0cb61e19084a355036bc4d3162ae1cfd4c88
Masoud Ramezani <masoudhra@gmail.com>
2023-11-28 12:25:57 +1300

    async in useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,12 +5,29 @@ const [counter, setCounter ] = useState(0)
 
 
 const increaseHandler = () =>  setCounter(counter + 1);
+const increaseHandler3 = () => {
+  // setCounter(counter + 3)
+  // agar taghir be meghdare tahire habli vabaste bood be in shekl ba elesh esteffade mikonim
+  setCounter(counter => counter + 1);
+  setCounter(counter => counter + 1);
+  setCounter(counter => counter + 1);
+};
 
+const stopHandler = () =>{
+  setCounter("-");
+};
+ 
   return (
     <div>
       <h1>Counter</h1>
       <p>{counter}</p>
-      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={increaseHandler}>Increase 1</button>
+      <br />
+      <br />
+      <button onClick={increaseHandler3}>Increase 3</button>
+      <br />
+      <br />
+      <button onClick={stopHandler}>Stop</button>
     </div>
   );
 };
