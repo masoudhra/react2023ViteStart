d107a249c3b30c310c38fd97dff95d82afd759ed
Masoud Ramezani <masoudhra@gmail.com>
2023-11-28 16:27:33 +1300

    handle onchane and value in form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
 function Form() {
-    const loginHandler = () =>{
-        console.log("Login")
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = () => {
+    console.log({email, password});
+  };
+
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={emailHandler}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={passwordHandler}
+      />
       <button onClick={loginHandler}>Login</button>
     </div>
   );
