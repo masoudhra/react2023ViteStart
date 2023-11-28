136053c4aab3c6db6d393efc85e6712051cc00cc
Masoud Ramezani <masoudhra@gmail.com>
2023-11-28 14:36:06 +1300

    show and hide with useState in UI

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,34 +1,48 @@
 import { useState } from "react";
 
 const Counter = () => {
-const [counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [isShow, setIsShow] = useState(true);
 
+  const increaseHandler = () => setCounter(counter + 1);
+  const increaseHandler3 = () => {
+    // setCounter(counter + 3)
+    // agar taghir be meghdare tahire habli vabaste bood be in shekl ba elesh esteffade mikonim
+    setCounter((counter) => counter + 1);
+    setCounter((counter) => counter + 1);
+    setCounter((counter) => counter + 1);
+  };
 
-const increaseHandler = () =>  setCounter(counter + 1);
-const increaseHandler3 = () => {
-  // setCounter(counter + 3)
-  // agar taghir be meghdare tahire habli vabaste bood be in shekl ba elesh esteffade mikonim
-  setCounter(counter => counter + 1);
-  setCounter(counter => counter + 1);
-  setCounter(counter => counter + 1);
-};
+  const stopHandler = () => {
+    setCounter("-");
+  };
 
-const stopHandler = () =>{
-  setCounter("-");
-};
- 
+  const showHandler = () => {
+    setIsShow(true);
+  };
+
+  const hideHandler = () => {
+    setIsShow(false);
+  };
+
   return (
-    <div>
-      <h1>Counter</h1>
-      <p>{counter}</p>
-      <button onClick={increaseHandler}>Increase 1</button>
-      <br />
-      <br />
-      <button onClick={increaseHandler3}>Increase 3</button>
-      <br />
-      <br />
-      <button onClick={stopHandler}>Stop</button>
-    </div>
+    <>
+      <button onClick={showHandler}>Show</button>
+      <button onClick={hideHandler}>hide</button>
+      {isShow ? (
+        <div>
+          <h1>Counter</h1>
+          <p>{counter}</p>
+          <button onClick={increaseHandler}>Increase 1</button>
+          <br />
+          <br />
+          <button onClick={increaseHandler3}>Increase 3</button>
+          <br />
+          <br />
+          <button onClick={stopHandler}>Stop</button>
+        </div>
+      ) : null}
+    </>
   );
 };
 
